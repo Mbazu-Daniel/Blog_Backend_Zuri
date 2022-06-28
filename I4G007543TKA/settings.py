4b8882eb1fdd94af105963c773b6e3d83392ba45
Mbazu Daniel <mbazudaniel97@gmail.com>
2022-06-28 20:48:16 +0100

    update file for links application

--- I4G007543TKA/settings.py
+++ I4G007543TKA/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
+    # external  application
+    "rest_framework",
     # internal application
-    "blog",
+    "blog","links",
 ]
 
 MIDDLEWARE = [
--- I4G007543TKA/urls.py
+++ I4G007543TKA/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('blog', include('blog.urls', namespace='blog')),	# blog/urls.py
+    path("api/links/", include("links.urls")),
 ]
