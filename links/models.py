0cb358f996df92bbaf2c2c9db0c0dc08e51d5510
Mbazu Daniel <mbazudaniel97@gmail.com>
2022-07-16 14:02:08 +0100

    create custom managers and add to models and create a view

--- links/models.py
+++ links/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-# Create your models here.
+
+from .managers import ActiveLinkManager
 
 User = get_user_model()
 
+# Create your models here.
 class Link(models.Model):
     target_url = models.URLField(max_length=200)
     description  = models.CharField(max_length=200)
@@ -11,4 +13,5 @@ class Link(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_date = models.DateTimeField()
     active = models.BooleanField(default=True)
-    
+    objects = models.Manager() 
+    public = ActiveLinkManager()
--- links/urls.py
+++ links/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views 
 
+from .views import ActiveLinkView, RecentLinkView
+
+
 app_name="link"
 
 urlpatterns = [
@@ -8,4 +11,9 @@ urlpatterns = [
     path("update/<int:pk>", views.PostUpdateApi.as_view(), name="api_update"),
     path("delete/<int:pk>", views.PostDeleteApi.as_view(), name="api_delete"),
     path("", views.PostListApi.as_view(), name="api_list"),
+    
+    path("active/", ActiveLinkView.as_view(), name='active_link'),
+    path("recent/", RecentLinkView.as_view(), name='recent_link'),
+
+
 ]
